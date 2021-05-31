31dec24167ae370af3153db736398abf1569e3a8
Abhinav Mehta <abhinavmehtabatala1@gmail.com>
2021-05-31 09:24:13 +0530

    Last Commit

--- main/notes/settings.py
+++ main/notes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes1.apps.Notes1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
